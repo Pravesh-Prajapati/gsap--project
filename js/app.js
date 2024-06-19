f44387c1b990d7d07cf7d886663f92e2a2eda173
Pravesh <147392058+Pravesh-Prajapati@users.noreply.github.com>
2024-06-20 00:12:17 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,143 @@
+// gsap.from("#img1",{
+//     opacity:0,
+//     duration:1,
+//     delay:0.2,
+//     y:60,
+// });
+// gsap.from("#img2",{
+//     opacity:0,
+//     duration:1,
+//     delay:0.2,
+//     x:60,
+// });
+// gsap.from("#img3",{
+//     opacity:0,
+//     duration:1,
+//     delay:0.2,
+//     y:-60,
+// });
+
+// gsap.from(".data",{
+//     opacity:0,
+//     y:50,
+//     scrollTrigger:{
+//         trigger:".data",
+//         scroller:"body",
+//         start:"top 80%",
+//         markers:true
+//     }
+
+// });
+// gsap.from(".solution",{
+//     opacity:0,
+//     duration:1,
+//     delay:0.2,
+//     y:60,
+//     scrollTrigger:{
+//         trigger:".solution",
+//         scroller:"body",
+//         start:"top 60%",
+//         markers:true
+//     }
+
+// });
+// gsap.from(".partener",{
+//     opacity:0,
+//     y:50,
+//     scrollTrigger:{
+//         trigger:".partener",
+//         scroller:"body",
+//         start:"top 80%",
+//         markers:true
+//     }
+// });
+
+// ========================================================================
+
+gsap.from("#img1",{
+    opacity:-1,
+    y:50,
+    duration:1,
+    delay:0,
+})
+gsap.from("#img2",{
+    opacity:0,
+    duration:1,
+    x:60,
+})
+gsap.from("#img3" ,{
+    y:60,
+    duration:1,
+    opacity:0,
+})
+gsap.from(".data",{
+    y:80,
+    opacity:0,
+    duration:0.7,
+    delay:0.2,
+    scrollTrigger:{
+        trigger:".data",
+        // markers:true,
+    }
+})
+
+gsap.from(".img4",{
+    y:80,
+    duration:0.7,
+    delay:0.2,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".img4",
+        // markers:true,
+    }
+})
+gsap.from(".img42",{
+    y:80,
+    duration:0.7,
+    delay:0.2,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".img42",
+        // markers:true,
+    }
+})
+gsap.from(".solution-data",{
+    y:80,
+    duration:0.7,
+    delay:0.2,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".solution-data",
+        // markers:true,
+    }
+})
+gsap.from(".solution-data2",{
+    y:80,
+    duration:0.7,
+    delay:0.2,
+    opacity:0,
+    scrollTrigger:{
+        trigger:".solution-data2",
+        // markers:true,
+    }
+})
+gsap.from(".partener" ,{
+    y:80,
+    duration:0.7,
+    delay:0.2,
+    opacity:0.7,
+    scrollTrigger:{
+        trigger:".partener",
+        // markers:true,
+    }
+})
+gsap.from(".data2",{
+    y:80,
+    opacity:0,
+    duration:0.7,
+    delay:0.2,
+    scrollTrigger:{
+        trigger:".data2",
+        // markers:true,
+    }
+})
